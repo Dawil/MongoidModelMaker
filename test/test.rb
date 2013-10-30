86f096499d046683012490956fc500ab6a1b3902
David Wilcox <dave@davidgwilcox.com>
2013-10-30 13:15:53 +0000

    added a simple unit test

---
+++ test/test.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+
+module MongoidModelMapper
+  class GeneratorTest < Test::Unit::TestCase
+    def setup
+      puts "setup"
+    end
+    
+    def test_something_simple
+      puts 'test'
+    end
+
+    def teardown
+      puts 'sudoku'
+    end
+  end
+end
