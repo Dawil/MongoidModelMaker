9a369f8ea54a007f9e647a2ffffed40a830e1690
David Wilcox <dave@davidgwilcox.com>
2013-10-30 14:06:26 +0000

    removed code that printed version

--- lib/mongoid_model_maker.rb
+++ lib/mongoid_model_maker.rb
@@ -2,6 +2,4 @@ require_relative "mongoid_model_maker/version"
 require_relative "mongoid_model_maker/generators/relation/relation_generator"
 
 module MongoidModelMaker
-  # Your code goes here...
-  puts "VERSION is #{MongoidModelMaker::VERSION}"
 end
