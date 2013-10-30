b4230bd0e4a4fd65399fb49e8d069a967c6bd06f
David Wilcox <dave@davidgwilcox.com>
2013-10-30 14:09:20 +0000

    it seems to be testing against an old version

--- lib/mongoid_model_maker.rb
+++ lib/mongoid_model_maker.rb
@@ -2,4 +2,5 @@ require_relative "mongoid_model_maker/version"
 require_relative "mongoid_model_maker/generators/relation/relation_generator"
 
 module MongoidModelMaker
+  puts "Don't mention the version #{MongoidModelMaker::VERSION}"
 end
