2270acb8d1fbd59e5b8e59705c7159ad038bbf62
David Wilcox <dave@davidgwilcox.com>
2013-11-06 03:05:11 +0000

    added test and lib code for running scaffold from yaml file

--- lib/yaml_loader.rb
+++ lib/yaml_loader.rb
@@ -1,12 +1,46 @@
 require 'yaml'
+require 'rails'
 
 module MongoidModelMaker
   class Models
-    attr_accessor :classes
+    attr_accessor :classes, :file
 
+    # load yaml file
     def initialize yaml_filename
-      file = YAML::load_file( yaml_filename )
-      puts file
+      @file = YAML::load_file( yaml_filename )
+      #@classes = yaml_to_classes @file
+    end
+
+    # run scaffolds
+    #   invokes scaffold generator many times
+    def run_scaffolds
+      @file.each_pair do |class_name, spec|
+        args = class_name + ' ' + spec["fields"].map do |field|
+          "#{field["name"]}:#{field["type"]}"
+        end.join(' ')
+        Rails::Generators.invoke "scaffold", args
+      end
+    end
+    # run relations
+    #   invokes relation generator many times
+    def run_relations
+    end
+    # run factories
+    #   invokes factory generator many times
+    def run_factories
+    end
+  private
+    # create model representation
+    def yaml_to_classes file_hash
+      # add the top level classes
+      classes = {}
+      file_hash.find_all(&:without_parents).each_pair do |class_name,spec|
+      end
+      # add the immediate children classes until they're all gone
+    end
+
+    def without_parents class_def
+      class_def["relation"].nil?
     end
   end
 end
--- test/yaml_loader_test.rb
+++ test/yaml_loader_test.rb
@@ -4,7 +4,21 @@ module MongoidModelMaker
   class YamlLoaderTest < MiniTest::Unit::TestCase
     def test_simple_load
       @models = MongoidModelMaker::Models.new File.join( __dir__, "fixtures/simple_model.yaml" )
-      assert @models.classes[:person][:first]
+      assert @models.classes[:person][:first] == :string
+      assert @models.classes[:person][:last]  == :string
+    end
+
+    def test_scaffolds_once
+      @models = MongoidModelMaker::Models.new File.join( __dir__, "fixtures/simple_model.yaml" )
+      Rails::Generators.expects(:invoke).with("scaffold", "person first:string last:string")
+      @models.run_scaffolds
+    end
+
+    def test_scaffolds_twice
+      @models = MongoidModelMaker::Models.new File.join( __dir__, "fixtures/double_model.yaml" )
+      Rails::Generators.expects(:invoke).with("scaffold", "person first:string last:string")
+      Rails::Generators.expects(:invoke).with("scaffold", "dog name:string breed:string")
+      @models.run_scaffolds
     end
   end
 end
