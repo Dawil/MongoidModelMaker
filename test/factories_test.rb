a9c6ee3e560343869686e56bdd4c93534052045e
David Wilcox <dave@davidgwilcox.com>
2013-11-07 06:15:40 +0000

    added real code to the factories tests

--- test/factories_test.rb
+++ test/factories_test.rb
@@ -8,27 +8,74 @@ module MongoidModelMaker
     setup :prepare_destination
 
     test "calls factory_girl generator correctly" do
-      Rails::Generators.stubs(:invoke).with("factory_girl:model", "person first:string last:string")
+      Rails::Generators.expects(:invoke).with( "factory_girl:model", %w(person first:string last:string) )
       run_generator %w(person first:string last:string)
     end
 
     test "adds singular references in parent factory" do
-      assert false
+      @file_helper.create_file "tmp/test/factories/person.rb", <<RUBY
+FactoryGirl.define do
+  factory :person do
+    first "MyString"
+    last "MyString"
+  end
+end
+RUBY
+      run_generator %w(dog name:string breed:string --parent=person)
+      assert_file "test/factories/person.rb", <<RUBY
+FactoryGirl.define do
+  factory :person do
+    dog { FactoryGirl.build( :dog ) }
+    first "MyString"
+    last "MyString"
+  end
+end
+RUBY
     end
 
     test "adds plural references in parent factory" do
-      assert false
+      @file_helper.create_file "tmp/test/factories/person.rb", <<RUBY
+FactoryGirl.define do
+  factory :person do
+    first "MyString"
+    last "MyString"
+  end
+end
+RUBY
+      run_generator %w(dog name:string breed:string --parent=person --plural=true)
+      assert_file "test/factories/person.rb", <<RUBY
+FactoryGirl.define do
+  factory :person do
+    after(:create) do |person|
+      FactoryGirl.create_list( :dog, 3, person: person )
+    end
+    first "MyString"
+    last "MyString"
+  end
+end
+RUBY
     end
 
     test "adds custom factory code" do
-      assert false
-    end
-
-    test "reads stdin twice" do
-      FactoriesGenerator.any_instance.stubs(:gets).returns("hello\n","world\n")
-      run_generator %w(person)
-      puts `tree`
-      assert_file "hello", "hello\nworld"
+      FactoriesGenerator.any_instance.stubs(:gets)
+        .returns("{ Faker::Name.first_name }\n", "{ Faker::Name.last_name }\n")
+      @file_helper.create_file "tmp/test/factories/person.rb", <<RUBY
+FactoryGirl.define do
+  factory :person do
+    first "MyString"
+    last "MyString"
+  end
+end
+RUBY
+      run_generator %w(person first:string last:string --read_factories=true)
+      assert_file "test/factories/person.rb", <<RUBY
+FactoryGirl.define do
+  factory :person do
+    first { Faker::Name.first_name }
+    last { Faker::Name.last_name }
+  end
+end
+RUBY
     end
   end
 end
